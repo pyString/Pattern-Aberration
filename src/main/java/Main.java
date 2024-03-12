b8375a769ade0efc8cb6ea5366415153416e3df1
pedro <pedropincer@outlook.com>
2024-03-11 23:02:28 -0300

    Descobri a lógica da coisa!!!
    Te explico melhor na quarta feira.
    
    Adicionei a primeira imagem dos puzzles;
    Arrumei a lógica do Menu Principal;
    Adicionei uma tela de texto de introdução;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,10 @@ public class Main {
     public static Texture title;
     public static Texture playBtnOFF;
     public static Texture playBtnON;
+    public static Texture shitPuzzle1;
+
+    public static Jaylib.Rectangle playBtnArea;
+    public static Jaylib.Vector2 mousePoint;
 
 
 
@@ -27,14 +31,17 @@ public class Main {
         //initialize objects
         InitWindow(500, 800, "Demo");
         SetTargetFPS(60);
-        currntScreen = GameScreen.TUTORIAL;
+        currntScreen = GameScreen.TITLE;
 
         //images
         title = LoadTexture("resources/Title_Stuff.png");
         playBtnOFF = LoadTexture("resources/Play_btn_off.png");
         playBtnON = LoadTexture("resources/Play_btn_on.png");
+        shitPuzzle1 = LoadTexture("resources/shitty_puzzle1.png");
 
         int timer = 0;
+        playBtnArea = new Jaylib.Rectangle(GetScreenWidth()/2.0f - playW/2.0f, GetScreenHeight()/2.0f - playH/2.0f, (float) playW, GetScreenHeight());
+        mousePoint = new Jaylib.Vector2(0.0f, 0.0f);
 
         //gameloop ou render method
         while(!WindowShouldClose())
@@ -46,7 +53,7 @@ public class Main {
                 case TITLE ->
                 {
                     int playX = GetScreenWidth()/2 - playW/2;
-                    if (GetMouseX() < playX + playW && GetMouseX() > playX && GetScreenHeight() - GetMouseY() < playY + playH && GetScreenHeight() - GetMouseY() < playY)
+                    if (CheckCollisionPointRec(mousePoint, playBtnArea))
                     {
                         DrawTexture(playBtnON, playX, playY, WHITE);
                         if (IsMouseButtonPressed(0))
@@ -63,6 +70,7 @@ public class Main {
                     timer ++;
                     if (timer >= 900){currntScreen = GameScreen.PHASE1;}
                 }
+
             }
 
 
@@ -93,6 +101,8 @@ public class Main {
                 }
                 case PHASE1 -> {
                     ClearBackground(RAYWHITE);
+
+                    DrawTexture(shitPuzzle1, 50, 150, WHITE);
                 }
             }
 
